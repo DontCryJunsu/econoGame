4c1acf4dd38fd2aebd1b7e935c43c90c7fcdf214
jedy100 <jedy100@naver.com>
2019-04-05 20:31:44 +0900

    Using Coroutine

---
+++ Unity2Djaeseong/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int RLmove;
+    public float ac;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        transform.Translate(0, ac, 0);
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(RLmove*Time.deltaTime,0,0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-RLmove*Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<PlayerScript>().ac = 1;
+            StartCoroutine(Wait());
+        }
+    
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3f);
+        GetComponent<PlayerScript>().ac = 0.2f;
+    }
+
+}
