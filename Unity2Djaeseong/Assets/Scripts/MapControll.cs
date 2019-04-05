89d762c51fd8368a9a67bfb8bf788ea2f80122b3
jedy100 <jedy100@naver.com>
2019-04-05 16:03:33 +0900

    c# practice

---
+++ Unity2Djaeseong/Assets/Scripts/MapControll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapControll : MonoBehaviour
+{
+    public GameObject Map;
+    public Transform Player;
+    private void Update()
+    {
+    
+    }
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+      
+        
+
+
+
+        if (other.gameObject.tag == "MapController")
+        {
+            Instantiate(Map, Player.position , Player.rotation);
+        }
+    }
+
+}
