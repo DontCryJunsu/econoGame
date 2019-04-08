a79e081951d2e8bfb3a63195d91533fa75378775
jedy100 <jedy100@naver.com>
2019-04-08 20:54:34 +0900

    hazardsmaker

---
+++ Practice/Assets/Scripts/Hazardsmaker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazardsmaker : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public float startWait = 1;
+    public float spawnWait = 0.75f;
+    public float waveWait = 2;
+    public Transform tr;
+    int a = 1;
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(1);
+
+        while (true)
+        {
+            for (int i = 0; i < 10; ++a)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = tr.position + new Vector3(Random.Range(-10,10),0,0);
+                Quaternion spawnRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+  
+            }
+            yield return new WaitForSeconds(waveWait);
+      
+        }
+    }
+}
