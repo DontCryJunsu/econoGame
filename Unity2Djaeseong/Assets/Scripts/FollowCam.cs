f61c419ed48e26bf1bb77169a4b3a4ea6ca75ef3
jedy100 <jedy100@naver.com>
2019-05-08 16:18:14 +0900

    ShootingFunction

--- Unity2Djaeseong/Assets/Scripts/FollowCam.cs
+++ Unity2Djaeseong/Assets/Scripts/FollowCam.cs
@@ -6,22 +6,14 @@ public class FollowCam : MonoBehaviour
 {
     public Transform targetTr;
     public float dist = 1.31f;
-    public float height = 1.629f;
-    public float dampTrace = 20.0f;
-    private Transform tr;
-    // Start is called before the first frame update
-    void Start()
-    {
-        tr = GetComponent<Transform>();
-        
-    }
-
+    
     // Update is called once per frame
 
     void LateUpdate()
     {
-        tr.position = Vector3.Lerp(tr.position, targetTr.position + (Vector3.down * dist) + (Vector3.back * height), Time.deltaTime * dampTrace);
+
+       transform.position = new Vector3(0, targetTr.position.y- dist, -10);
                                  
-        tr.LookAt(targetTr.position);
+        
     }
 }
